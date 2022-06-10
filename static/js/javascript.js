f61a618f793116211535cde851a9d52baa141220
Laurynas <laurynas.jakstas@gmail.com>
2022-06-10 12:23:37 +0300

    Initial commit

--- static/js/javascript.js
+++ static/js/javascript.js
@@ -1,3 +1,6 @@
+document.getElementsByClassName("main")[0].addEventListener("click", navbar_hide);
+
+
 function navbar_hide() {
   const menuToggle = document.getElementById('navbarContent');
   menuToggle.setAttribute("class", "navbar-collapse collapse");
