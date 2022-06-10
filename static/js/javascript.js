9102464a94cbd1f876131be80f99587a7df6d3d5
Laurynas <laurynas.jakstas@gmail.com>
2022-06-10 16:33:36 +0300

    Initial commit

--- static/js/javascript.js
+++ static/js/javascript.js
@@ -1,7 +1,14 @@
-
+const main = document.getElementsByClassName('main')[0];
+main.addEventListener('click', navbar_hide2)
 
 
 function navbar_hide() {
   const navContent = document.getElementById('navbarContent');
   navContent.setAttribute("class", "navbar-collapse collapse");
+}
+
+function navbar_hide2() {
+  const navContent = document.getElementById('navbarContent');
+  const bsCollapse = new bootstrap.Collapse(navContent, {toggle:false})
+  bsCollapse.hide()
 }
