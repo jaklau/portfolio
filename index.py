118ae0ac8098556b1023cb1d93597a3cca3c7b72
Laurynas Jakstas <113884625+jaklau@users.noreply.github.com>
2023-08-04 17:12:14 +0300

    Update index.py

--- index.py
+++ index.py
@@ -4,6 +4,7 @@ import os
 phone = os.environ.get("PHONE")
 email = os.environ.get("EMAIL")
 fa_kit = os.environ.get("FA_KIT")
+cf_sitekey = os.environ.get("CF_SITEKEY")
 
 
 app = Flask(__name__)
@@ -11,7 +12,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return render_template("index.html", phone=phone, email=email, fa_kit=fa_kit)
+    return render_template("index.html", phone=phone, email=email, fa_kit=fa_kit, cf_sitekey=cf_sitekey)
 
 
 if __name__ == "__main__":
