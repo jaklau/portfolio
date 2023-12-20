f42f57be10e3b943c2c1c92916278dd39bb6e6f5
laurynas.jakstas <laurynas.jakstas@hostinger.com>
2023-12-20 13:38:40 +0200

    test: Lint

--- index.py
+++ index.py
@@ -1,20 +1,20 @@
 from flask import Flask, render_template
 import os
 
-phone = os.environ.get("PHONE")
-email = os.environ.get("EMAIL")
-fa_kit = os.environ.get("FA_KIT")
-cf_sitekey = os.environ.get("CF_SITEKEY")
 
+phone = os.environ.get('PHONE')
+email = os.environ.get('EMAIL')
+fa_kit = os.environ.get('FA_KIT')
+cf_sitekey = os.environ.get('CF_SITEKEY')
 
 
 app = Flask(__name__)
 
-@app.route("/")
+
+@app.route('/')
 def home():
-    return render_template("index.html", phone=phone, email=email, fa_kit=fa_kit, cf_sitekey=cf_sitekey)
+    return render_template('index.html', phone=phone, email=email, fa_kit=fa_kit, cf_sitekey=cf_sitekey)
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     app.run()
-
